ff60a235c65a3cdcff8fad66b176c3a85bf0c6ea
khs <khs@123.com>
2023-11-30 22:03:49 +0800

    Make delivery option interactive

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -70,3 +70,18 @@ export function updateQuantity(productId, newQuantity){
   
   saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+  let matchingItem;
+
+  //匹配购物车中需要更新的Product
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
