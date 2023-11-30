ed7d8e84e14fb715f0ac979a2c25a0e9a9b9c738
khs <khs@123.com>
2023-11-30 22:10:38 +0800

    Regenerate HTML when updating delivery option

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -4,89 +4,33 @@ import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
 
-let cartSummaryHTML = '';
+function renderOrderSummary(){
 
-cart.forEach((cartItem) => {
+  let cartSummaryHTML = '';
 
-  const productId = cartItem.productId;
+  cart.forEach((cartItem) => {
 
-  let matchingProduct;
+    const productId = cartItem.productId;
 
-  //利用id匹配product
-  products.forEach((product) => {
-    if(product.id === productId){
-      matchingProduct = product;
-    }
-  });
+    let matchingProduct;
 
-  const deliveryOptionId = cartItem.deliveryOptionId;
+    //利用id匹配product
+    products.forEach((product) => {
+      if(product.id === productId){
+        matchingProduct = product;
+      }
+    });
 
-  let deliveryOption;
+    const deliveryOptionId = cartItem.deliveryOptionId;
 
-  deliveryOptions.forEach((option) => {
-    if(deliveryOptionId === option.id){
-      deliveryOption = option;
-    }
-  });
+    let deliveryOption;
 
-  const today = dayjs();
-  const deliveryDate = today.add(
-    deliveryOption.deliveryDays,
-    'days'
-  );
-  const dateString = deliveryDate.format(
-    'dddd, MMMM D'
-  );
+    deliveryOptions.forEach((option) => {
+      if(deliveryOptionId === option.id){
+        deliveryOption = option;
+      }
+    });
 
-  cartSummaryHTML += 
-  `
-  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-    <div class="delivery-date">
-      Delivery date: ${dateString}
-    </div>
-
-    <div class="cart-item-details-grid">
-      <img class="product-image"
-        src="${matchingProduct.image}">
-
-      <div class="cart-item-details">
-        <div class="product-name">
-          ${matchingProduct.name}
-        </div>
-        <div class="product-price">
-          $${formatCurrency(matchingProduct.priceCents)}
-        </div>
-        <div class="product-quantity">
-          <span>
-            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-          </span>
-          <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
-            Update
-          </span>
-          <input class="quantity-input">
-          <span class="link-primary save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
-          
-          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-            Delete
-          </span>
-        </div>
-      </div>
-
-      <div class="delivery-options">
-        <div class="delivery-options-title">
-          Choose a delivery option:
-        </div>
-        ${deliveryOptionsHTML(matchingProduct, cartItem)}
-      </div>
-    </div>
-  </div>
-  `;
-});
-
-function deliveryOptionsHTML(matchingProduct, cartItem){
-  let html = '';
-
-  deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
     const deliveryDate = today.add(
       deliveryOption.deliveryDays,
@@ -96,101 +40,164 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
       'dddd, MMMM D'
     );
 
-    const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`;
-
-    //判断当前deliveryOption.id是否是当前cartItem.deliveryOptionId
-    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
-
-    html += 
+    cartSummaryHTML += 
     `
-      <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
-        <input type="radio"
-          ${isChecked ? 'checked' : ''}
-          class="delivery-option-input"
-          name="delivery-option-${matchingProduct.id}">
-        <div>
-          <div class="delivery-option-date">
-            ${dateString}
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date: ${dateString}
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
           </div>
-          <div class="delivery-option-price">
-            ${priceString} - Shipping
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
+              Update
+            </span>
+            <input class="quantity-input">
+            <span class="link-primary save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
+            
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
           </div>
         </div>
-      </div>
-    `
-  })
 
-  return html;
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          ${deliveryOptionsHTML(matchingProduct, cartItem)}
+        </div>
+      </div>
+    </div>
+    `;
+  });
+
+  function deliveryOptionsHTML(matchingProduct, cartItem){
+    let html = '';
+
+    deliveryOptions.forEach((deliveryOption) => {
+      const today = dayjs();
+      const deliveryDate = today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      );
+      const dateString = deliveryDate.format(
+        'dddd, MMMM D'
+      );
+
+      const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`;
+
+      //判断当前deliveryOption.id是否是当前cartItem.deliveryOptionId
+      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+      html += 
+      `
+        <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
+          <input type="radio"
+            ${isChecked ? 'checked' : ''}
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">
+              ${dateString}
+            </div>
+            <div class="delivery-option-price">
+              ${priceString} - Shipping
+            </div>
+          </div>
+        </div>
+      `
+    })
+
+    return html;
+  }
+
+  document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+  updateCheckOutQuantity();
+
+  //删除link
+  document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      //从界面移除HTML
+      container.remove();
+      updateCheckOutQuantity();
+    });
+  });
+
+  //update-link
+  document.querySelectorAll('.js-update-link').forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      
+      //显示输入框和Save, 隐去Update
+      container.querySelector('.quantity-input').classList.add('is-editing-quantity');
+      container.querySelector('.save-quantity-link').classList.add('is-editing-quantity');
+      container.querySelector('.js-update-link').classList.add('is-not-editing');
+
+      updateCheckOutQuantity();
+    });
+  });
+
+  //Save-link
+  document.querySelectorAll('.save-quantity-link').forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      const newQuantity = Number(container.querySelector('.quantity-input').value);
+      if(typeof newQuantity ==='number' && newQuantity > 0 && newQuantity < 1000){
+        updateQuantity(productId, newQuantity);
+        //更新Quantity
+        renderOrderSummary();
+      } 
+      
+      //隐藏输入框和Save, 显示Update
+      container.querySelector('.quantity-input').classList.remove('is-editing-quantity');
+      container.querySelector('.save-quantity-link').classList.remove('is-editing-quantity');
+      container.querySelector('.js-update-link').classList.remove('is-not-editing');
+
+      container.querySelector('.quantity-input').innerHTML = '';
+      updateCheckOutQuantity();
+    });
+  });
+
+  //刷新顶部checkOut数量
+  function updateCheckOutQuantity(){
+    //计算购物车总数
+    let cartQuantity =  calculateCartQuantity();
+
+    //更新显示的checkOut数量
+    document.querySelector('.js-checkout-quantity').innerHTML = `${cartQuantity} items`;
+  };
+
+  document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary();
+    });
+  });
+
 }
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-updateCheckOutQuantity();
-
-//删除link
-document.querySelectorAll('.js-delete-link').forEach((link) => {
-  link.addEventListener('click', () => {
-    const productId = link.dataset.productId;
-    removeFromCart(productId);
-
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-    //从界面移除HTML
-    container.remove();
-    updateCheckOutQuantity();
-  });
-});
-
-//update-link
-document.querySelectorAll('.js-update-link').forEach((link) => {
-  link.addEventListener('click', () => {
-    const productId = link.dataset.productId;
-
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-    
-    //显示输入框和Save, 隐去Update
-    container.querySelector('.quantity-input').classList.add('is-editing-quantity');
-    container.querySelector('.save-quantity-link').classList.add('is-editing-quantity');
-    container.querySelector('.js-update-link').classList.add('is-not-editing');
-
-    updateCheckOutQuantity();
-  });
-});
-
-//Save-link
-document.querySelectorAll('.save-quantity-link').forEach((link) => {
-  link.addEventListener('click', () => {
-    const productId = link.dataset.productId;
-
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-
-    const newQuantity = Number(container.querySelector('.quantity-input').value);
-    if(typeof newQuantity ==='number' && newQuantity > 0 && newQuantity < 1000){
-      updateQuantity(productId, newQuantity);
-      //更新Quantity
-      container.querySelector('.quantity-label').innerHTML = newQuantity;
-    } 
-    
-    //隐藏输入框和Save, 显示Update
-    container.querySelector('.quantity-input').classList.remove('is-editing-quantity');
-    container.querySelector('.save-quantity-link').classList.remove('is-editing-quantity');
-    container.querySelector('.js-update-link').classList.remove('is-not-editing');
-
-    container.querySelector('.quantity-input').innerHTML = '';
-    updateCheckOutQuantity();
-  });
-});
-
-//刷新顶部checkOut数量
-function updateCheckOutQuantity(){
-  //计算购物车总数
-  let cartQuantity =  calculateCartQuantity();
-
-  //更新显示的checkOut数量
-  document.querySelector('.js-checkout-quantity').innerHTML = `${cartQuantity} items`;
-};
-
-document.querySelectorAll('.js-delivery-option').forEach((element) => {
-  element.addEventListener('click', () => {
-    const {productId, deliveryOptionId} = element.dataset;
-    updateDeliveryOption(productId, deliveryOptionId);
-  });
-});
+renderOrderSummary();
